79fb54bcf126762bc973586db1a4baca0e32e295
Muhammet ali Coşgun <macosgun7134@gmail.com>
2026-04-05 12:09:07 +0300

    genel

--- src/components/AnnouncementBar.tsx
+++ src/components/AnnouncementBar.tsx
@@ -1,4 +1,4 @@
-const DEFAULT_TEXT = "💸 İLK SİPARİŞİNİZE %10 İNDİRİM 💸      📦 850₺ ÜZERİ KARGO ÜCRETSİZ 📦      ";
+const DEFAULT_TEXT = "🏷️ İLK SİPARİŞE %10 İNDİRİM 🏷️      📦 850₺ ÜZERİ KARGO ÜCRETSİZ 📦      ";
 const DEFAULT_COLOR = "#111111";
 
 const AnnouncementBar = () => {
